9bd4dc6ace8bbb26ca97d494391f2262f45063a8
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2022-03-04 23:03:49 +0600

    HTML file from the task1 Added at the resources>views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,150 @@
-<h1>Hello World!</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Root Next Food Store </title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link rel="icon" href="data/icon2.png" type="image/x-icon">
+<script type="text/javascript" src="index.js"> </script>
+</head>
+<body>
+ <body onload="change_cover()">
+    <div class ="navi_bar" id="navi_bar">
+    <table border="1"> 
+        <tr> 
+            <th width="15%" rowspan="2"><a href="#"><img src="data/icon.png" style=" max-height: 70px;" ></a></td>
+            <th width="40%">
+                <input type="text" name="search" id="search_box" placeholder="Search Foods" size="25%">
+                <input type="button" value="Search" size="10%"  onclick="search_box()">
+            </th>
+            <th width="20%">
+                <a href="www.facebook.com">
+                    <img src="data/facebook-logo.png" alt="fb" id="social_1" onmouseover="zoom_s_icon(id)" onmouseout="zoom_s_icon_remove(id)"></a> &nbsp;
+                <a href="www.instagram.com">
+                    <img src="data/instagram.png" alt="insta" id="social_2" onmouseover="zoom_s_icon(id)" onmouseout="zoom_s_icon_remove(id)"></a>&nbsp;
+                <a href="www.twitter.com">
+                    <img src="data/twitter.png" alt="twitter" id="social_3" onmouseover="zoom_s_icon(id)" onmouseout="zoom_s_icon_remove(id)"></a> &nbsp;
+                <a href="wwww.youtube.com">
+                    <img src="data/youtube.png" alt="yt" id="social_4" onmouseover="zoom_s_icon(id)" onmouseout="zoom_s_icon_remove(id)"></a> &nbsp;
+
+            </th>
+            <th width="15%">
+                <select name="Language" id="Language" onchange="change_lang()">
+                    <option value="English"> English</option>
+                    <option value="বাংলা "> Bangla</option>
+                    
+                </select>  
+            </th>        
+        <th width="10%" onclick="show_alert('login funtion under maintaince')"> login  </th>
+        </tr>
+        <tr>
+
+            <th colspan="3" style="background-color: #e4f2e1;">
+                <marquee scrollamount="2" height="30px"> 
+                    Upto 50% discount for Mirpur.&emsp;&emsp;&emsp;&emsp;
+                    Cash on delivery available in Dhaka city.. &emsp;&emsp;&emsp;&emsp;    
+                    Fresh and organic ... &emsp;&emsp;&emsp;&emsp;
+                    
+                </marquee>
+            </th>
+            <th width="10%"> <a href="#">
+                <img src="data/cart.png"> <label id="cart"> 0</label> </a>
+            </th>
+        </tr>
+    </table>
+    </div>
+</div>
+
+    <div class="space" id="space"> <br></div>
+    <table border="1" style="border: #3dd61b;">
+        <tr>
+            <td width="20%"> 
+                <div class="cato_left">
+                <h3 style="text-align:center;background-color: #3dd61b;"> <i> Categories </i></h3>
+                <ul class="link"> 
+                    <a href="index.html" >
+                        <li id="l1" onmouseenter="font_size_plus(id)" onmouseout="font_size_minus(id)"> Sea foods</li> </a>
+                    <a href="index.html"> 
+                        <li id="l2" onmouseenter="font_size_plus(id)" onmouseout="font_size_minus(id)"> Vegitable</li> </a>
+                    <a href="index.html"> 
+                        <li id="l3" onmouseenter="font_size_plus(id)" onmouseout="font_size_minus(id)"> Fruits</li>    </a>
+                    <a href="index.html"> 
+                        <li id="l4" onmouseenter="font_size_plus(id)" onmouseout="font_size_minus(id)"> Meats</li>     </a>
+                    <a href="index.html"> 
+                        <li id="l5" onmouseenter="font_size_plus(id)" onmouseout="font_size_minus(id)"> Special</li>   </a>
+                </ul>
+            </div>
+            </td>
+            <td width="80%"> 
+                <img name="cover_img" id="cover_img" src="data/cover_0.jpg" class="item_list" alt="banner img">
+            </td>
+        </tr>
+    </table>
+<div class="cat_title"><br> <label id="cat_title_lable" name="cat_title" style="display:block;">Featured Produts</label> </div>
+        <div class="cat_title_2"> 
+            <a href="#"> Top Selling </a> |
+            <a href="#"> Gift Items </a> |
+            <a href="#"> Free delivery </a> |
+            <a href="#"> Organic </a> |
+            <a href="#"> Fast foods </a> 
+        </div>
+        <br>
+        <table border="1" class="item_list">
+        <tr width="20%">
+            <td width="20%" onclick="open_popup('Carrot 1kg','199.00')"> 
+                <img src="data/carrot.png" class="item_list"> গাজর  1kg<br>taka 40.00 </td>
+            <td width="20%" onclick="open_popup('Potato 1kg','299.00')"> 
+                <img src="data/banana.png" class="item_list"> কলা 1 Dozen<br>taka 50.00 </td>
+            <td width="20%" onclick="open_popup('Pineapple 1kg','65.00')"> 
+                <img src="data/Pineapple.png" class="item_list"> আনারস  1pc<br>taka 150.00</td>
+            <td width="20%" onclick="open_popup('Mushrooms 1kg','300.00')"> 
+                <img src="data/mushrooms.png" class="item_list"> মাশরুম  1kg<br>taka 250.00 </td>
+            <td width="20%" onclick="open_popup('Cucumbers 1kg','250.00')"> 
+                <img src="data/cucumbers.png" class="item_list"> শশা 1kg<br>taka 50.00</td>
+ 
+
+        </tr>
+    </table> 
+
+
+    <div class="footer"> 
+        <br> <br>
+        <a href="https://www.rootnext.com/vision/"> Home </a> |
+        <a href="#"> FAQ </a>  |
+        <a href="https://www.rootnext.com/about-us/"> About Us </a>  |
+        <a href="https://www.rootnext.com/contact-us/"> Contact Us </a> <br> <br>
+
+            Copyright (c) 2022 <a href="https://www.rootnext.com/"> RootNext_Solution </a>
+        <p> Coded By Md.Shamsuzzaman , Icons from <a href="https://www.rootnext.com/">Flat Icon</a> 
+           
+    </div>
+</div>    
+
+
+    
+    <div class="popup_item" id="popup_item">
+        <div class="popup_item_h">
+                <label style="text-align: center;" id="popup_item_titile"> </label>
+                <input type="button" value="X" style="font-weight: bold ;text-align: right;background-color: rgb(247, 157, 157);" onclick="close_popup()">
+        </div>
+        <div class="popup_item_b">
+            Price : Lkr. 
+            <label id="popup_item_price"> </label> /=
+            <br><br>
+            <label> Quntity : </label>
+            <input type="number" id="qty" value=1 style="width: 50px;background-color:#e1f14d;font-weight: bold;">
+        </div>
+        <div class="popup_item_buttions">
+            <input type="button" id="add2cart" onclick="add2cart()"value="Add to cart" class="popup_item_buttions_2" style="align-items: left;">
+            &nbsp;
+            <input type="button" id="buynow" onclick="buynow()" value=" Buy Now " class="popup_item_buttions_2" style="align-items: right;">
+        </div>
+    </div>
+    <div  id="overlay"></div>
+
+
+</body>
+</html>
+
+
+</body>
+</html>
